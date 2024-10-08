d8dc96f6f68cba7090324e96075a911e6dae1552
ShiruvatiNarasimha <mrnattyboy05@gmail.com>
2024-10-08 18:15:28 +0530

    Add Navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,9 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
+import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,10 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Navbar />
+      <body className={inter.className}>
+        <div className="relative w-full flex items-center justify-center">
+          <Navbar />
+        </div>
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
-
 export default function Home() {
-  return <div className="text-3xl text-center mt-10">Hi There</div>;
+  return (
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]"></main>
+  );
 }
