8aaf24b68c86b780cb0973e74ae611d4718458c5
ShiruvatiNarasimha <mrnattyboy05@gmail.com>
2024-10-09 19:16:58 +0530

    Add Testimonial card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Features from "@/components/Featured";
 import HeroSection from "@/components/HeroSection";
+import MusicSchoolTestimonials from "@/components/TestimonialCard";
 import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
       <HeroSection />
       <Features />
       <WhyChooseUs />
+      <MusicSchoolTestimonials />
     </main>
   );
 }
---
+++ src/components/TestimonialCard.tsx
@@ -0,0 +1,56 @@
+"use client";
+import { InfiniteMovingCards } from "./ui/infinite-moving-cards";
+
+const musicSchoolTestimonials = [
+  {
+    quote:
+      "Cut bill  pay time by 50% Automate Manual bill data entry, approvals, payouts and reconciliation to cut bill pay time at least 50%.",
+    name: "Alex Johnson",
+    title: "",
+  },
+  {
+    quote:
+      "Save 1000+ man hours per month Increase your finance operations efficiency and spend more time on high value-add tasks. ",
+    name: "Samantha Lee",
+    title: "Piano Student",
+  },
+  {
+    quote:
+      "This school offered me the tools and confidence to take my singing to the next level. I'm endlessly grateful for the personalized coaching.",
+    name: "Michael Chen",
+    title: "Vocal Student",
+  },
+  {
+    quote:
+      "As a violinist, finding the right mentor can be challenging, but this school matched me with a teacher who truly understands my goals and challenges.",
+    name: "Emily Taylor",
+    title: "Violin Student",
+  },
+  {
+    quote:
+      "The production courses here opened my eyes to the intricacies of music production. Highly recommend for any aspiring producers!",
+    name: "Chris Morales",
+    title: "Music Production Student",
+  },
+];
+
+function MusicSchoolTestimonials() {
+  return (
+    <div className="h-[40rem] w-full dark:bg-black dark:bg-grid-white/[0.2] relative flex flex-col items-center justify-center overflow-hidden">
+      <h2 className="text-3xl font-bold text-center mb-8 z-10">
+        Why finance leaders choose Peakflo
+      </h2>
+      <div className="flex justify-center w-full overflow-hidden px-4 sm:px-6 lg:px-8">
+        <div className="w-full max-w-6xl">
+          <InfiniteMovingCards
+            items={musicSchoolTestimonials}
+            direction="right"
+            speed="slow"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MusicSchoolTestimonials;
