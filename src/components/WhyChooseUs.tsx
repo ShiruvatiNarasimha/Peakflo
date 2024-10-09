5c4eab6f2e0eb520048d8940f58a47684db96890
ShiruvatiNarasimha <mrnattyboy05@gmail.com>
2024-10-09 18:32:20 +0530

    Add WhyChooseUs

---
+++ src/components/WhyChooseUs.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import React from "react";
+
+import { StickyScroll } from "./ui/sticky-scroll-reveal";
+
+const musicSchoolContent = [
+  {
+    title: "Discover Your Sound with Us: A Personal Journey in Music Mastery",
+    description:
+      "Embark on a musical journey that’s uniquely yours. Our personalized instruction adapts to your individual needs, setting the stage for unparalleled growth and creativity. At our music school, your aspirations meet our dedicated support, creating a harmonious path to mastery.",
+  },
+  {
+    title: "Discover Your Sound with Us: A Personal Journey in Music Mastery",
+    description:
+      "Embark on a musical journey that’s uniquely yours. Our personalized instruction adapts to your individual needs, setting the stage for unparalleled growth and creativity. At our music school, your aspirations meet our dedicated support, creating a harmonious path to mastery.",
+  },
+  {
+    title: "Discover Your Sound with Us: A Personal Journey in Music Mastery",
+    description:
+      "Embark on a musical journey that’s uniquely yours. Our personalized instruction adapts to your individual needs, setting the stage for unparalleled growth and creativity. At our music school, your aspirations meet our dedicated support, creating a harmonious path to mastery.",
+  },
+  {
+    title: "Cutting-Edge Curriculum",
+    description:
+      "Our curriculum is continuously updated to include the latest music education trends and technologies, ensuring you’re always learning with the most current and effective methods. Say goodbye to outdated materials and welcome an education that evolves with the industry.",
+  },
+  {
+    title: "Cutting-Edge Curriculum",
+    description:
+      "Our curriculum is continuously updated to include the latest music education trends and technologies, ensuring you’re always learning with the most current and effective methods. Say goodbye to outdated materials and welcome an education that evolves with the industry.",
+  },
+  {
+    title: "Limitless Learning Opportunities",
+    description:
+      "With our expansive resource library and dynamic course offerings, you’ll never find yourself without something new to explore. Our platform provides continuous opportunities for growth, ensuring your musical skills are always advancing.",
+  },
+];
+
+function WhyChooseUs() {
+  return (
+    <div>
+      <StickyScroll content={musicSchoolContent} />
+    </div>
+  );
+}
+
+export default WhyChooseUs;
